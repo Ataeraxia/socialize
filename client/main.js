4f6b23819bbede453a3796d7646986a8cc52fd81
Ataeraxia <you@example.com>
2018-02-26 13:55:47 -0500

    Add navbar and link to /

--- client/main.js
+++ client/main.js
@@ -4,6 +4,9 @@ import { render } from "react-dom";
 
 import App from "../imports/ui/App.js";
 
+// Matt Lieber
+//    |
+//   \/
 Meteor.startup(() => {
   render(<App />, document.getElementById("render-target"));
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 export default class App extends Component {
     render() {
         return (
-            <div className="App" />
+            <div className="App"> 
+                <div className="navbar">
+                    <h1>
+                        <a href="/">socialize</a>
+                    </h1>
+                </div>
+            </div>
         );
     }
 }
