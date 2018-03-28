9b3aa0d33ddd0380a7bc1ce0b6740754159fbe77
Ataeraxia <you@example.com>
2018-03-28 15:53:44 -0400

    Get a date object to render.

--- imports/ui/Contact.js
+++ imports/ui/Contact.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 
 export default class Contact extends Component {
-  render() {
+  renderDetails() {
     return (
-      <li>{this.props.contact.name}</li>
+      <ul>
+        <li>
+          {this.props.contact.details.lastContact.toLocaleString()}
+        </li>
+      </ul>
+    )
+  }
+  render() {
+    const isDetails = this.props.contact.details;
+
+    const detailsList = isDetails ? this.renderDetails() : "";
+
+    return (
+      <li>
+        <h2>
+          {this.props.contact.name}
+        </h2>
+        {detailsList}
+      </li>
     );
   }
 }
