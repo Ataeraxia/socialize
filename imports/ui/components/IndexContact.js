5fbdb6b7183541b3781e923c82e090f00717af4d
Ataeraxia <you@example.com>
2018-04-01 12:26:39 -0400

    Make link wrap entire index contact and fix css.

--- imports/ui/components/IndexContact.js
+++ imports/ui/components/IndexContact.js
@@ -6,20 +6,20 @@ import { formatDate } from "../../api/utils.js";
 export default class IndexContact extends Component {
   render() {
     return (
-      <li className="index-contact">
-        <h2>
-          <div className="name">
-            <Link to={"/" + this.props.indexContact._id}>
-              {this.props.indexContact.name}
-            </Link>
-          </div>
-          {this.props.indexContact.details.lastContact && (
-            <div className="index-last-contact">
-              <h3>{formatDate(this.props.indexContact.details.lastContact)}</h3>
-            </div>
-          )}
-        </h2>
-      </li>
+      <Link className="index-contact" to={"/" + this.props.indexContact._id}>
+        <li>
+          <h2>
+            <span className="name">{this.props.indexContact.name}</span>
+            {this.props.indexContact.details.lastContact && (
+              <div className="index-last-contact">
+                <h3>
+                  {formatDate(this.props.indexContact.details.lastContact)}
+                </h3>
+              </div>
+            )}
+          </h2>
+        </li>
+      </Link>
     );
   }
 }
