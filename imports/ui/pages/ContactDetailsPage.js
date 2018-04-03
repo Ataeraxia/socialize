151b53049ddff2ab518defbf7145f0edca062e65
Ataeraxia <you@example.com>
2018-04-03 09:45:33 -0400

    Add withRouter to try and fix reload problem. Render contact details.

--- imports/ui/pages/ContactDetailsPage.js
+++ imports/ui/pages/ContactDetailsPage.js
@@ -4,12 +4,20 @@ import { withTracker } from "meteor/react-meteor-data";
 
 import { Contacts } from "../../api/contacts.js";
 
+import ContactDetails from "../components/ContactDetails.js";
+
 class ContactDetailsPage extends Component {
   render() {
     const { match, location, history, contact } = this.props;
-    return <div className="page">I like butts, {contact.name}</div>;
+    return (
+      <div className="page">
+        <ContactDetails details={contact.details} />
+      </div>
+    );
   }
 }
+
+const ContactDetailsPageWithRouter = withRouter(ContactDetailsPage);
 
 export default withTracker(props => {
   Meteor.subscribe("contacts");
@@ -17,4 +25,4 @@ export default withTracker(props => {
   return {
     contact: Contacts.findOne({ _id: props.match.params.contactId })
   };
-})(ContactDetailsPage);
+})(ContactDetailsPageWithRouter);
